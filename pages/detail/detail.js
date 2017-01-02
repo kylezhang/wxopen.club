bb96e49375c4a6d7e790a583d295c4f5c929d990
zhangkai <succpeking@hotmail.com>
2017-01-02 22:51:06 +0800

    使用http://wxopen.club/的api学习微信小程序开发。

---
+++ pages/detail/detail.js
@@ -0,0 +1,45 @@
+Page({
+  data: {
+    post : {},
+    id: ''
+  },
+  onLoad: function(options) {
+    //Do some initialize when page load.
+    console.log('===detail====:', options.id)
+    this.setData({ id: options.id })
+  },
+  onReady: function() {
+    //Do some when page show.
+  },
+  onShow: function() {
+    //Do some when page ready.
+    const that = this;
+    const id   = this.data.id;
+
+    wx.request({
+      url: `http://wxopen.club/api/v1/topic/${id}?mdrender=false`,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res)
+        that.setData({
+          post: res.data.data
+        })
+      }
+    })
+    
+  },
+  onHide: function() {
+    //Do some when page hide.
+    
+  },
+  onUnload: function() {
+    //Do some when page unload.
+    
+  },
+  onPullDownRefresh: function() {
+    //Do some when page pull down.
+    
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,76 @@
+// const moment = require('../../node_modules/moment/src/moment');
+// 处理时间的小工具函数
+// const stamptime2str = (date) => {
+//   return moment(date).format('YYYY-MM-DD HH')
+// };
+Page({
+  data: {
+    arr: null,
+    opts: {
+      page: 1,
+      tab: '',
+      limit: 10,
+      mdrender: false
+    }
+  },
+  onLoad: function(options) {
+    //Do some initialize when page load.
+    
+  },
+  onReady: function() {
+    //Do some when page ready.
+    
+  },
+  onShow: function() {
+    //Do some when page show.
+    this.fetchData()
+  },
+  onHide: function() {
+    //Do some when page hide.
+    
+  },
+  onUnload: function() {
+    //Do some when page unload.
+    
+  },
+  onPullDownRefresh: function() {
+    //Do some when page pull down.
+    this.fetchData()
+  },
+  //同意获取数据函数
+  fetchData: function() {
+    let that   = this;
+    const {page, tab, limit} = this.data.opts;
+
+    wx.request({
+      url: `http://wxopen.club/api/v1/topics?page=${page}&tab=${tab}&limit=${limit}`,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        // console.log('frome cnodejs data====：', res.data)
+        if(res.data.success){
+          let resData = res.data.data
+          that.setData({
+            arr: resData
+          })
+        }
+      }
+    })
+  },
+  //滚动触底加载新数据
+  lower: function(e){
+    console.log('====')
+    this.setData({
+      'opts.page' : this.data.opts.page + 1
+    })
+
+    this.fetchData()
+  },
+  gotoDetail: function(e){
+    const id = e.currentTarget.id;
+    wx.navigateTo({
+      url: `../detail/detail?id=${id}`
+    })
+  }
+})
